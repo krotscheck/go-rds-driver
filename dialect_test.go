1ae2d0fe301d62b8e041628de7a67d95a5eed361
Ville Lehto <ville.lehto@nitorcreations.com>
2026-05-22 08:28:14 +0300

    Add tests for UTC conversion in ConvertNamedValue timestamp formatting

--- dialect_test.go
+++ dialect_test.go
@@ -2,6 +2,8 @@ package rds_test
 
 import (
 	"database/sql/driver"
+	"time"
+
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/rdsdata/types"
 	"github.com/krotscheck/go-rds-driver"
@@ -12,6 +14,35 @@ import (
 func Test_Dialect(t *testing.T) {
 
 	Convey("ConvertNamedValue", t, func() {
+
+		Convey("time.Time is formatted as UTC regardless of input timezone", func() {
+			helsinki, _ := time.LoadLocation("Europe/Helsinki") // UTC+2/UTC+3
+			t := time.Date(2024, 3, 15, 14, 30, 45, 123456000, helsinki)
+
+			namedValue := driver.NamedValue{Name: "ts", Value: t}
+			result, err := rds.ConvertNamedValue(namedValue)
+
+			So(err, ShouldBeNil)
+			So(result, ShouldResemble, types.SqlParameter{
+				Name:     aws.String("ts"),
+				TypeHint: types.TypeHintTimestamp,
+				Value:    &types.FieldMemberStringValue{Value: "2024-03-15 12:30:45.123456"},
+			})
+		})
+
+		Convey("time.Time already in UTC is formatted unchanged", func() {
+			t := time.Date(2024, 3, 15, 12, 30, 45, 0, time.UTC)
+
+			namedValue := driver.NamedValue{Name: "ts", Value: t}
+			result, err := rds.ConvertNamedValue(namedValue)
+
+			So(err, ShouldBeNil)
+			So(result, ShouldResemble, types.SqlParameter{
+				Name:     aws.String("ts"),
+				TypeHint: types.TypeHintTimestamp,
+				Value:    &types.FieldMemberStringValue{Value: "2024-03-15 12:30:45"},
+			})
+		})
 
 		Convey("Null Values", func() {
 			var UInt8 []uint8
